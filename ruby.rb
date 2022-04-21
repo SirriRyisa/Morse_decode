9c88eb0f4c7a208cc0cf99616a5fa68b3f959fca
SirriRyisa <sirriryisa@gmail.com>
2022-04-21 09:24:20 +0100

    Morse decoder

---
+++ ruby.rb
@@ -0,0 +1,62 @@
+# Decoding the symbols into letters
+Morse_name = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z',
+  '----' => '0',
+  '.----' => '1',
+  '..---' => '2',
+  '...--' => '3',
+  '....-' => '4',
+  '.....' => '5',
+  '-....' => '6',
+  '--...' => '7',
+  '---..' => '8',
+  '----.' => '9'
+}
+
+#Getting characters
+
+def decode_char(letter)
+  Morse_name[letter]
+end
+
+#Getting words from the characters
+
+def decode_words(word)
+  letters = word.split
+  letters.map { |letter| decode_char(letter) }.join
+end
+
+p decode_words('.-. -.-- ..')
+
+def decode_message(message)
+  words = message.split('   ')
+  words.map { |word| decode_words(word) }.join(' ')
+end
+
+p decode_message('.-. -.-- ..   .-. -.-- ..')
+
